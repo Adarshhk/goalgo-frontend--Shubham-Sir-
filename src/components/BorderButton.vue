4a64cd579756018e08a9d29e619b61beb84604e2
Adarsh Kaushal <Adarshkaushal14@gmail.com>
2024-10-03 11:23:44 +0530

    update

--- src/components/BorderButton.vue
+++ src/components/BorderButton.vue
@@ -4,6 +4,7 @@ defineProps({
 })
 </script>
 
+
 <template>
     <button class="border border-algo-orange text-algo-orange px-8 py-3 rounded-xl w-full hover:bg-algo-orange hover:text-black transition duration-200 ease-in-out">
         {{ title }}
